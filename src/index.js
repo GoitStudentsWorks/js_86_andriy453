154b97a19b48fead4b92f6f6052444a1025cab34
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-08-25 23:39:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import { popularRecipesRendering } from './components/PopularRecipes/PopularRecipes.js';
-import './components/Header/Header.js'
+import './components/Header/Header.js';
 import './components/Header/header-menu.js'
 import './components/Categories/Categories.js'
 import './components/Hero/swiper.js'
